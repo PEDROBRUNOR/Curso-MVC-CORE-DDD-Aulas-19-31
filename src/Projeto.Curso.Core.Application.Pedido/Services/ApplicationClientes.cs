af70426445aa2c5807d218616b361be20dee6450
Pedro Bruno <pedrobrunor@hotmail.com>
2018-09-15 09:39:53 -0300

    aula 31

---
+++ src/Projeto.Curso.Core.Application.Pedido/Services/ApplicationClientes.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using Projeto.Curso.Core.Application.Pedido.Interfaces;
+using Projeto.Curso.Core.Application.Pedido.ViewModels;
+using Projeto.Curso.Core.Domain.Pedido.Entidades;
+using Projeto.Curso.Core.Domain.Pedido.Interfaces.Services;
+using Projeto.Curso.Core.Infra.CrossCutting.Extensions;
+using System;
+using System.Collections.Generic;
+
+namespace Projeto.Curso.Core.Application.Pedido.Services
+{
+    public class ApplicationClientes : IApplicationClientes
+    {
+        private readonly IServiceClientes serviceclientes;
+        private readonly IMapper mapper;
+
+        public ApplicationClientes(IServiceClientes _serviceclientes,
+                                   IMapper _mapper)
+        {
+            serviceclientes = _serviceclientes;
+            mapper = _mapper;
+        }
+
+        public ClientesViewModel Adicionar(ClientesViewModel cliente)
+        {
+            return mapper.Map<ClientesViewModel>(serviceclientes.Adicionar(mapper.Map<Clientes>(cliente)));
+        }
+
+        public ClientesViewModel Atualizar(ClientesViewModel cliente)
+        {
+            return mapper.Map<ClientesViewModel>(serviceclientes.Atualizar(mapper.Map<Clientes>(cliente)));
+        }
+
+        public ClientesViewModel Remover(ClientesViewModel cliente)
+        {
+            return mapper.Map<ClientesViewModel>(serviceclientes.Remover(mapper.Map<Clientes>(cliente)));
+        }
+        public IEnumerable<ClientesViewModel> ObterTodos()
+        {
+            return mapper.Map<IEnumerable<ClientesViewModel>>(serviceclientes.ObterTodos());
+        }
+
+        public ClientesViewModel ObterPorId(int id)
+        {
+            return mapper.Map<ClientesViewModel>(serviceclientes.ObterPorId(id));
+        }
+
+        public ClientesViewModel ObterPorApelido(string apelido)
+        {
+            return mapper.Map<ClientesViewModel>(serviceclientes.ObterPorApelido(apelido));
+        }
+
+        public ClientesViewModel ObterPorCpfCnpj(string cpfcnpj)
+        {
+            return mapper.Map<ClientesViewModel>(serviceclientes.ObterPorCpfCnpj(cpfcnpj.SomenteNumeros()));
+        }
+
+        public void Dispose()
+        {
+            serviceclientes.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
---
+++ src/Projeto.Curso.Core.Site/Areas/Cadastros/Controllers/ClientesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Projeto.Curso.Core.Application.Pedido.Interfaces;
+
+namespace Projeto.Curso.Core.Site.Areas.Cadastros.Controllers
+{
+    [Area("Cadastros")]
+    public class ClientesController : Controller
+    {
+
+        private readonly IApplicationClientes appclientes;
+
+        public ClientesController(IApplicationClientes _appclientes)
+        {
+            appclientes = _appclientes;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult ListagemClientesJson()
+        {
+            var lista = appclientes.ObterTodos();
+            var settings = new JsonSerializerSettings();
+            return Json(lista, settings);
+        }
+
+        public IActionResult Incluir()
+        {
+            return View();
+        }
+
+    }
+}
